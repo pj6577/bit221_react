da23de3247b846827c803168764da427086046e0
pj6577 <pj6577@naver.com>
2022-07-21 16:39:57 +0900

    first test m 20220721

---
+++ CH00_준비하기/Javascript/ex01_consol.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+var a = 1, b=2, d="Sting"
+console.log(a, b, d);
+
+console.log( typeof(a), typeof(b), typeof(d));
+console.log("값은 %d입니다", a, "타입은 ",typeof(a), "입니다");
+
+                                              
+
+
+
+
+
+
