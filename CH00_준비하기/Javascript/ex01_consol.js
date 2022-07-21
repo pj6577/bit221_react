7e6b558da9b42232f00dd9b86147e003e8cacafc
pj6577 <pj6577@naver.com>
2022-07-21 16:43:00 +0900

    first test 1_2 m 20220721

--- CH00_준비하기/Javascript/ex01_consol.js
+++ CH00_준비하기/Javascript/ex01_consol.js
@@ -4,11 +4,5 @@ console.log(a, b, d);
 
 console.log( typeof(a), typeof(b), typeof(d));
 console.log("값은 %d입니다", a, "타입은 ",typeof(a), "입니다");
-
+//
                                               
-
-
-
-
-
-
